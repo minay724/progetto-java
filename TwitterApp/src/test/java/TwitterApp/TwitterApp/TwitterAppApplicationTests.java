539b05bfdddc37b02d4bcac5a3c71879da25b306
NelloZ <f.nello01@libero.it>
2022-01-18 14:28:07 +0100

    JUnit primo test

--- TwitterApp/src/test/java/TwitterApp/TwitterApp/TwitterAppApplicationTests.java
+++ TwitterApp/src/test/java/TwitterApp/TwitterApp/TwitterAppApplicationTests.java
@@ -1,13 +1,23 @@
 package TwitterApp.TwitterApp;
-
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import TwitterApp.TwitterApp.Modello.Tweet;
 @SpringBootTest
 class TwitterAppApplicationTests {
-
-	@Test
+/**
+ * da tweet testiamo 
+ */
+	Tweet tweetTest = new Tweet();
+	Map<String, Integer> mentionsTest = new HashMap<String, Integer>();
+	
+	@BeforeEach
 	void contextLoads() {
+		tweetTest.setText("@fcBarca @messi @fcBarca ciao");
+		tweetTest.countMentions();
+		
+		mentionsTest.put("fcBarca", 2);
+		mentionsTest.put("messi", 1);
 	}
-
 }
