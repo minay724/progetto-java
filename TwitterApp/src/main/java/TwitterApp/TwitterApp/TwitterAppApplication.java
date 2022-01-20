d9a40128e0ec75963d824e83fecddd709812d6e4
Mina <s1100870@studenti.univpm.it>
2022-01-20 22:10:57 +0100

    update javadoc

--- TwitterApp/src/main/java/TwitterApp/TwitterApp/TwitterAppApplication.java
+++ TwitterApp/src/main/java/TwitterApp/TwitterApp/TwitterAppApplication.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-/**
- * 
- * classe main
- */
+
 public class TwitterAppApplication {
-	/**
-	 * 
-	 * @param args 
-	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterAppApplication.class, args);
 	}
